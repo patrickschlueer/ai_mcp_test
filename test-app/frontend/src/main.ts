f20a755f1c08830309dee0c798416c75807bcfc2
patrickschlueer <patrickschlueer@web.de>
2025-10-27 03:26:21 +0100

    update: test-app/frontend/src/main.ts - STRUKTURELLER KONFLIKT: Standalone Components beibehalten, kein app.module.ts erstellen

--- test-app/frontend/src/main.ts
+++ test-app/frontend/src/main.ts
@@ -1,9 +1,33 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { userReducer } from './app/store/user/user.reducer';
+import { filterReducer } from './app/store/filter/filter.reducer';
+import { UserEffects } from './app/store/user/user.effects';
+import { FilterEffects } from './app/store/filter/filter.effects';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient()
+    provideRouter(routes),
+    provideStore({
+      users: userReducer,
+      filters: filterReducer
+    }),
+    provideEffects([UserEffects, FilterEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      trace: false,
+      traceLimit: 75
+    }),
+    importProvidersFrom(HttpClientModule)
   ]
 }).catch(err => console.error(err));
